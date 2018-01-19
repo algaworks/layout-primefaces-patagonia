63e7217140df48cfca2720ef5d16db46c6fb2134
Renan Resende <renan-resende@ultracar.com.br>
2018-01-19 14:05:34 -0200

    Adicionando sub itens no menu lateral.

--- src/main/webapp/resources/algaworks/javascripts/app.js
+++ src/main/webapp/resources/algaworks/javascripts/app.js
@@ -1,6 +1,13 @@
 $(function() {
 	$('.js-toggle').bind('click', function(event) {
+		debugger;
 		$('.js-sidebar, .js-content').toggleClass('is-toggled');
 		event.preventDefault();
-	});	
+	});
+	
+	$('.aw-item-link').bind('click', function(event) {
+		debugger;
+		$('.aw-menu-tree').toggle();
+		event.preventDefault();
+	});
 });
