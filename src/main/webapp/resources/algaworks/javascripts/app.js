48da01804ee56fdd803ae91976365d72d01dfe06
Renan Resende <rmnresende@gmail.com>
2018-01-20 16:24:48 -0200

    Fim refatoração subitens.

--- src/main/webapp/resources/algaworks/javascripts/app.js
+++ src/main/webapp/resources/algaworks/javascripts/app.js
@@ -11,35 +11,44 @@ $(function() {
 
 function expandirRecolherItensDoMenu() {
 
-	debugger;
 	event.preventDefault();
 	var linkClicado = event.target;
 
-	// se clicou no icone do link em vez do link
+	// se clicou no icone do link em vez de clicar no link
 	if ($(linkClicado).hasClass('fa-fw')) {
 		linkClicado = $(linkClicado).parent();
 	}
 
-	var liPai = $(linkClicado).parent();
-
-	//se ja estiver selecionada e for subitem nao precisa fazer nada
-	if (!($(liPai).hasClass('is-selected')) && !($(liPai).hasClass('subitem'))) {
-		desativarLiSelecionada();
-		expandirRecolherMenu(linkClicado, liPai);
-	}
+	avaliarQualLiRecolherExpandir($(linkClicado).parent(), linkClicado);
 }
 
 function expandirRecolherMenu(linkClicado, liPai) {
+
 	$(liPai).toggleClass('is-selected');
 	var proximoMenu = $(linkClicado).next('.aw-menu-tree');
 	proximoMenu.toggle('1000');
 }
 
 function desativarLiSelecionada() {
+
 	var liAtiva = $('.is-selected');
+
 	if (liAtiva.length > 0) {
 		var menuExpandido = $(liAtiva).find('.aw-menu-tree');
 		menuExpandido.toggle('1000');
 		$(liAtiva).removeClass('is-selected');
+	}
+}
+
+function avaliarQualLiRecolherExpandir(liPai, linkClicado){
+
+	//quando for subitem nao precisa fazer nada
+	if (!($(liPai).hasClass('subitem'))) {
+
+		//se nao estiver selecionada, desativa a li que estiver selecionda
+		if(!($(liPai).hasClass('is-selected'))){
+			desativarLiSelecionada();
+		}
+		expandirRecolherMenu(linkClicado, liPai);
 	}
 }
